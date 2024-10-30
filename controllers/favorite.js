7198976f786f61fbe3be7ca075cfe23f6fb37e00
paolasoria <paola.soria0597@gmail.com>
2024-10-29 23:56:32 -0400

    enhancing favorites response to have author info

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -7,7 +7,12 @@ import Recipe from "../models/model-recipe.js"
 export const getFavorites = async (req, res) => {
     try {
         const userId = req.user._id;
-        const favorites = await Favorites.findOne({owner: userId}).populate('recipes').populate('owner')
+        const favorites = await Favorites.findOne({owner: userId}).populate({
+            path: 'recipes',
+            populate: {
+                path: 'author',
+            }
+        })
         if (!favorites) {
             res.status(404).json({
                 message: "Favorites for User Not Found"
